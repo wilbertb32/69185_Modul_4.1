75307b9fb18de393cc11909f593fa6091d6adf6a
wilbertb32 <wilbertbernardi2004@gmail.com>
2025-02-25 21:33:54 +0700

    Modul 4.1 Step 6

--- app/src/main/java/com/example/compose/rally/ui/overview/OverviewBody.kt
+++ app/src/main/java/com/example/compose/rally/ui/overview/OverviewBody.kt
@@ -61,6 +61,13 @@ import com.example.compose.rally.ui.components.RallyDivider
 import com.example.compose.rally.ui.components.formatAmount
 import com.example.compose.rally.ui.theme.RallyTheme
 import java.util.Locale
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.ui.unit.Dp
 
 @Composable
 fun OverviewBody(onScreenChange: (RallyScreen) -> Unit = {}) {
@@ -100,18 +107,17 @@ private fun AlertCard() {
         // Start the animation
         currentTargetElevation = 8.dp
     }
-    val animatedElevation = animateDpAsState(
-        targetValue = currentTargetElevation,
-        animationSpec = tween(durationMillis = 500),
-        finishedListener = {
-            currentTargetElevation = if (currentTargetElevation > 4.dp) {
-                1.dp
-            } else {
-                8.dp
-            }
-        }
+    val infiniteElevationAnimation = rememberInfiniteTransition()
+    val animatedElevation: Dp by infiniteElevationAnimation.animateValue(
+        initialValue = 1.dp,
+        targetValue = 8.dp,
+        typeConverter = Dp.VectorConverter,
+        animationSpec = infiniteRepeatable(
+            animation = tween(500),
+            repeatMode = RepeatMode.Reverse
+        )
     )
-    Card(elevation = animatedElevation.value) {
+    Card(elevation = animatedElevation) {
         Column {
             AlertHeader {
                 showDialog = true
